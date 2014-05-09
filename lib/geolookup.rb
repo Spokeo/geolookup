603f81073f72635af1446faff2d7817e51d3f106
Eric Kuecks <ekuecks@g.ucla.edu>
2014-05-08 18:40:59 -0700

    2014-05-08  Eric Kuecks  <ekuecks.ucla.edu>
    	* lib/data: Added this directory. Inside are yaml files containing
    	all necessary hashes for the gem.
    
    	* lib/geolookup.rb: Added self.load_hash_from_file function to
    	load a hash from a yaml file.
    
    	* lib/geolookup/usa/county.rb: Removed constant hashes and replaced with
    	instance fields to be loaded from the yaml files in /lib/data.
    
    	* lib/geolookup/usa/state.rb: Removed constant hashes and replaced with
    	instance fields	to be loaded from the yaml files in /lib/data.
    
    	* lib/geolookup/country.rb: Removed constant hashes and replaced with
    	instance fields to be loaded from the yaml files in /lib/data.

--- lib/geolookup.rb
+++ lib/geolookup.rb
@@ -4,5 +4,15 @@ require "geolookup/usa/state"
 require "geolookup/country"
 
 module Geolookup
-
+  ###################################################################
+  # self.load_file
+  #
+  # Loads a given .yml file that contains a hash and returns the hash
+  #
+  def self.load_hash_from_file(file)
+    fileName = File.open(file, "r")
+    hash = YAML.load(fileName)     
+    fileName.close
+    hash
+  end
 end
