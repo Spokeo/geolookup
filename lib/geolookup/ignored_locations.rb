990f618f4d9930e506b017bdb36c617c414eadbb
Qi Li <qili@mb108-qli.spokeo.lan>
2017-06-12 11:29:44 -0700

    SB4399-ignorelocations

---
+++ lib/geolookup/ignored_locations.rb
@@ -0,0 +1,16 @@
+require 'yaml'
+
+module Geolookup
+
+  module IgnoredLocations
+    IGNORED_LOCATIONS_FILE = "IGNORED_LOCATIONS.yml"
+
+    @ignored_locations
+
+    def self.ignored_locations
+      @ignored_locations ||= Geolookup.load_hash_from_file(IGNORED_LOCATIONS_FILE)
+      @ignored_locations.keys
+    end
+
+  end
+end
