cac118a8c9b89f01a0d31d1ecd88f67833330bac
randyvillanueva <rvillanueva@spokeo.com>
2018-02-13 14:38:52 -0800

    add find_city_by_zip

--- lib/geolookup/usa/city.rb
+++ lib/geolookup/usa/city.rb
@@ -3,6 +3,8 @@ module Geolookup
     module City
 
       CITY_MAP = 'CITIES.yml'
+      ZIP_CITY = 'ZIP_CITY.yml'
+
 
       @cities_by_name
 
@@ -10,6 +12,12 @@ module Geolookup
         @cities_by_name ||= Geolookup.load_hash_from_file(CITY_MAP)
 
         @cities_by_name[city_name.to_s.downcase] || []
+      end
+
+      def self.find_city_by_zip(zip)
+        @zip_city_map ||= Geolookup.load_hash_from_file(ZIP_CITY)
+
+        @zip_city_map[zip.to_s]
       end
 
     end
