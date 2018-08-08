e2dfd7d2d431e75f4751b2ed8015239cd1419ed6
Reed Dadoune <rdadoune@spokeo.com>
2018-08-08 12:28:40 -0700

    Handle nil case

--- lib/geolookup/usa/metro.rb
+++ lib/geolookup/usa/metro.rb
@@ -16,8 +16,8 @@ module Geolookup
       def self.code_to_name(metro_code, v2 = false)
         @metro_code_to_name ||= Geolookup.load_hash_from_file(METRO_CODE_TO_NAME_FILE)
         name = get_value_from_hash(@metro_code_to_name, metro_code.to_s.to_i)
-        name.tr('-', '_') unless v2
-        name
+        return name.tr('-', '_') if !v2 && name
+        return name
       end
 
       ###################################################################
