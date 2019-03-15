322a54ad83472d9915dbb0dbc556d1dd4d79a7c8
Juan Banda <jbanda@spokeo.com>
2019-03-15 11:41:16 -0700

    Sb 7653 areacodes (#20)
    * areacode population methods
    
    * passing specs
    
    * renamed methods to be more explicit
    
    * added comments
    
    * better field/variable names
    
    * um
    
    * removed bad test

--- lib/geolookup/usa/areacodes.rb
+++ lib/geolookup/usa/areacodes.rb
@@ -3,14 +3,103 @@ module Geolookup
     module AreaCodes
 
       AREA_CODE_META_INFO_FILE = 'AREA_CODE_META_INFO.yml'
+      AREA_CODE_POPULATION_FILE = 'CITY_STATE_COUNTY_POPULATION.csv'
 
       def self.to_h
         @area_codes_hash ||= Geolookup.load_hash_from_file(AREA_CODE_META_INFO_FILE)
       end
 
+      def self.details
+        @records ||= Geolookup.load_list_from_csv(AREA_CODE_POPULATION_FILE).map do |r|
+          Detail.new(area_code: r[0].to_i,
+                     city: r[1],
+                     state_code: r[2].to_i,
+                     county_code: r[3].to_i,
+                     population: r[4].to_i)
+        end
+      end
+
       def self.find(area_code)
         to_h[area_code.to_s] || {}
       end
+
+      # Return a list of detail records for area_code,
+      # sorted by population in descending order.
+      def self.major_cities(area_code)
+        @major_cities_by_area_code ||= create_major_cities_by_area_code
+        @major_cities_by_area_code.fetch(area_code.to_i, [])
+      end
+
+      # Return a list of area_codes for city and state_code.
+      def self.area_codes_by_city_and_state_code(city, state_code)
+        @area_codes_by_city_and_state ||= create_area_codes_by_city_and_state_code
+        @area_codes_by_city_and_state.fetch(state_code).fetch(city.downcase, [])
+      end
+
+      # Return a list of area_codes for county_code and state_code
+      def self.area_codes_by_county_code_and_state_code(county_code, state_code)
+        @area_codes_by_county_and_state ||= create_area_codes_by_county_code_and_state_code
+        @area_codes_by_county_and_state.fetch(state_code).fetch(county_code, [])
+      end
+
+      private
+
+      def self.create_area_codes_by_county_code_and_state_code
+        h = {}
+        details.group_by{ |r| r.state_code }.each do |state, records|
+          counties = {}
+          h[state] = counties
+          records.group_by{ |r| r.county_code }.each do |county, countyrecords|
+            counties[county] = countyrecords.map{ |r| r.area_code }.sort.uniq
+          end
+        end
+        h
+      end
+
+      def self.create_major_cities_by_area_code
+        m = {}
+        details.group_by { |r| r.area_code }.each do |code, records|
+          m[code] = records.sort_by{ |r| [-r.population, r.city, r.state_code] }
+        end
+        m
+      end
+
+      def self.create_area_codes_by_city_and_state_code
+        h = {}
+        details.group_by{ |r| r.state_code }.each do |state, records|
+          cities = {}
+          h[state] = cities
+          records.group_by{ |r| r.city }.each do |city, cityrecords|
+            cities[city.downcase] = cityrecords.map{ |r| r.area_code }.uniq.sort
+          end
+        end
+        h
+      end
+
+      class Detail
+        attr_reader :area_code, :city, :state_code, :county_code, :population
+
+        def initialize(area_code:, city:, state_code:, county_code:, population:)
+          @area_code = area_code
+          @city = city
+          @state_code = state_code
+          @county_code = county_code
+          @population = population
+        end
+
+        def state_name
+          State.code_to_name(state_code)
+        end
+
+        def state_abbrev
+          State.code_to_abbreviation(state_code)
+        end
+
+        def county_name
+          County.code_to_name(state_code, county_code)
+        end
+      end
+
     end
   end
 end
--- spec/lib/areacodes_spec.rb
+++ spec/lib/areacodes_spec.rb
@@ -1,4 +1,4 @@
-describe 'Geolookup::USA::AreaCodes' do
+describe Geolookup::USA::AreaCodes do
 
 
   describe 'info' do
@@ -14,4 +14,85 @@ describe 'Geolookup::USA::AreaCodes' do
       expect(Geolookup::USA::AreaCodes.find(2023)).to eql({})
     end
   end
+
+  describe '.details' do
+    it 'is not empty' do
+      expect(described_class.details).to_not be_empty
+    end
+
+    it 'is over 9000' do # https://www.youtube.com/watch?v=SiMHTK15Pik
+      expect(described_class.details.size>9000).to be_truthy
+    end
+
+    it 'uses same list' do
+      expect(described_class.details.object_id)
+        .to eql(described_class.details.object_id)
+    end
+
+    it 'has valid records' do
+      described_class.details.each do |r|
+        expect(r.area_code>200).to be_truthy, "Failed area_code: #{r}"
+        expect(r.city).to_not be_empty, "Failed city: #{r}"
+        expect(r.state_code>0).to be_truthy, "Failed state: #{r}"
+        expect(r.county_code>0).to be_truthy, "Failed county: #{r}"
+        expect(r.population>0).to be_truthy, "Failed population: #{r}"
+      end
+    end
+
+  end
+
+  describe '.major_cities' do
+    it 'returns cities sorted by population descending' do
+      cities = described_class.major_cities(949)
+      expected_records = Geolookup::USA::AreaCodes.details.select do |r|
+        r.area_code == 949
+      end.sort_by {|r| -r.population }
+      expect(cities).to eql(expected_records)
+    end
+  end
+
+  describe '.area_codes_by_city_and_state_code' do
+    it 'returns area codes for anaheim' do
+      codes = described_class.area_codes_by_city_and_state_code('anaheim', 6)
+      expect(codes).to eql([714, 657].sort)
+    end
+
+    it 'returns area codes for anaheim - capitalized' do
+      codes = described_class.area_codes_by_city_and_state_code('ANAHEIM', 6)
+      expect(codes).to eql([714, 657].sort)
+    end
+  end
+
+  describe '.area_codes_by_county_code_and_state_code' do
+    it 'returns area codes for anaheim' do
+      codes = described_class.area_codes_by_county_code_and_state_code(59, 6)
+      expect(codes).to eql([562, 657, 714, 949].sort)
+    end
+
+  end
+
+  describe 'Detail' do
+    let(:record) do
+      described_class.details.find do |r|
+        r.state_code==6 && r.county_code == 59
+      end
+    end
+
+    context 'state' do
+      it 'returns state_name' do
+        expect(record.state_name).to eql('California')
+      end
+
+      it 'returns state_abbrev' do
+        expect(record.state_abbrev).to eql('CA')
+      end
+    end
+
+    context 'county' do
+      it 'returns county-name' do
+        expect(record.county_name).to eql('ORANGE')
+      end
+    end
+  end
+
 end
